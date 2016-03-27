5e956c4e2ba7ecf7d9067d05775c333c24bbc132
yorickdewid <yorick17@outlook.com>
2016-03-27 23:42:36 +0200

    Only log when debug is defined

--- Amendux/Log.h
+++ Amendux/Log.h
@@ -8,22 +8,30 @@ namespace Amendux {
 		static Log *s_Log;
 
 		Log() {
+#ifdef DEBUG
 			_log.open("Amendux.log");
 			_log << "[Log] Initialize logger module" << std::endl;
+#endif
 		}
 
 		~Log() {
+#ifdef DEBUG
 			_log << "[Log] Terminate logger module" << std::endl;
 			_log.close();
+#endif
 		}
 
 	public:
 		void write(const std::wstring& mod, const std::wstring& s) {
+#ifdef DEBUG
 			_log << "[" << mod << "] " << s << std::endl;
+#endif
 		}
 
 		void write(const wchar_t *mod, const wchar_t *s) {
+#ifdef DEBUG
 			_log << "[" << mod << "] " << s << std::endl;
+#endif
 		}
 
 		static void Init() {
